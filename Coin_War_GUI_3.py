b999826a0e0fa1e8a37fa52bfce4b8c3a7babe64
StevenChen22 <sc22@pdx.edu>
2014-08-03 22:42:51 -0700

    Still trying to make a GUI

--- Coin_War_GUI_3.py
+++ Coin_War_GUI_3.py
@@ -5,22 +5,6 @@
 import random
 from tkinter import *
 
-# Hands (if using hardcoded values, change random_hands to False)
-hand1_army = []
-hand1_prisoners = []
-hand2_army = []
-hand2_prisoners = []
-random_hands = True
-
-def flip(hand_army):
-    """Flips coin 5 times and adds H or T to hand"""
-    for i in range(5):
-        j = random.randrange(2)
-        if j == 1:
-            hand_army += "H"
-        elif j == 0:
-            hand_army += "T"
-
 def capture(winner_army, winner_prisoners, loser_army, loser_prisoners):
     """Distributes coins between players armies and prisoners after a capture"""
     winner_army.extend(loser_army[0])
@@ -35,44 +19,43 @@ def capture(winner_army, winner_prisoners, loser_army, loser_prisoners):
     winner_army.extend(winner_prisoners)
     del winner_prisoners[:]
 
-def display_results():
-    """Prints each players army followed by their prisoners"""
-    print ("Player 1's army consists of:", ', '.join(hand1_army))
-    print("Player 1's prisoners consist of:", ', '.join(hand1_prisoners))
-
-    print("Player 2's army consists of:", ', '.join(hand2_army))
-    print("Player 2's prisoners consist of:", ', '.join(hand2_prisoners))
-
-class Application(Frame):
+class CoinWar(Frame):
     """ GUI application that creates a story based on user input. """
-    def __init__(self, master):
+    def __init__(self, root):
         """ Initialize Frame. """
-        super(Application, self).__init__(master)
-        self.grid()
-        self.create_widgets()
+        super().__init__(root)
 
-    def create_widgets(self):
+        self.grid()
+
         """ Create widgets to get story information and to display story. """
         # Create instruction label
         Label(self,
               text = "Coin War"
-              ).grid(row = 0, column = 0, columnspan = 2, sticky = W)
+              ).grid(row = 0, column = 1, columnspan = 2, sticky = E + W)
 
         # Create a button for rules
         Button(self,
                text = "Click for rules",
                command = self.show_rules
-               ).grid(row = 1, column = 0, sticky = W)
+               ).grid(row = 1, column = 0, sticky = E + W)
 
         # Create a button for game play
         Button(self,
                text = "Click to play",
                command = self.play_game
-               ).grid(row = 2, column = 0, sticky = W)
+               ).grid(row = 2, column = 0, sticky = E + W)
 
-        # Display program area
-        self.game_txt = Text(self, width = 75, height = 10, wrap = WORD)
-        self.game_txt.grid(row = 7, column = 0, columnspan = 4)
+        # Display program label
+        self.game_txt = label(self, justify = LEFT)
+        self.game_txt.grid(row = 3, column = 0, columnspan = 4)
+
+    def display_results(self):
+        """Prints each players army followed by their prisoners"""
+        self.game_txt += ("Player 1's army =", self.join(self.hand1_army))
+        self.game_txt += ("Player 1's prisoners =", self.join(self.hand1_prisoners))
+
+        self.game_txt += ("Player 2's army =", self.join(self.hand2_army))
+        self.game_txt += ("Player 2's prisoners =", self.join(self.hand2_prisoners))
 
     def show_rules(self):
         """ Fill text box with the rules."""
@@ -107,77 +90,96 @@ that player wins.
 Otherwise, the game is a tie.
 """
 
-    # display the rules
-    self.game_txt.delete(0.0, END)
-    self.game_txt.insert(0.0, rules)
+        # display the rules
+        self.game_txt.delete(0.0, END)
+        self.game_txt.insert(0.0, rules)
 
     def play_game(self):
         """Play the game!"""
-        text = ""
-        # Flip and fill hands
-        if random_hands == True:
-            flip(hand1_army)
-            flip(hand2_army)
+        # Hands (if using hardcoded values, change self.random_hands to False)
+        self.hand1_army = []
+        self.hand1_prisoners = []
+        self.hand2_army = []
+        self.hand2_prisoners = []
+        self.random_hands = True
+        self.game_txt = ""
 
-        text += display_results()
+        if self.random_hands == True:
+            """Flips coin 5 times and adds H or T to hand1_army"""
+            for i in range(5):
+                j = random.randrange(2)
+                if j == 1:
+                    self.hand1_army += "H"
+                elif j == 0:
+                    self.hand1_army += "T"
+            """Flips coin 5 times and adds H or T to hand2_army"""
+            for i in range(5):
+                j = random.randrange(2)
+                if j == 1:
+                    self.hand2_army += "H"
+                elif j == 0:
+                    self.hand2_army += "T"
+
+        self.game_txt += display_results(self)
+
         # Main loop
         n = True
         while n == True:
 
             # Break if either team has an empty army
-            if len(hand1_army) == 0 or len(hand2_army) == 0:
+            if len(self.hand1_army) == 0 or len(self.hand2_army) == 0:
                 n == False
                 break
 
             # Hand 1 is winner
-            elif hand1_army[0] == "H" and hand2_army[0] == "T":
-                capture(hand1_army, hand1_prisoners, hand2_army, hand2_prisoners)
-                text += "Player 1 wins this hand!\n"
-                text += display_results()
+            elif self.hand1_army[0] == "H" and self.hand2_army[0] == "T":
+                    capture(hand1_army, hand1_prisoners, hand2_army, hand2_prisoners)
+                    self.game_txt += "Player 1 wins this hand!\n"
+                    self.game_txt += display_results(self)
 
             # Hand 2 is winner
-            elif hand1_army[0] == "T" and hand2_army[0] == "H":
-                capture(hand2_army, hand2_prisoners, hand1_army, hand1_prisoners)
-                text += "Player 2 wins this hand!\n"
-                text += display_results()
+            elif self.hand1_army[0] == "T" and self.hand2_army[0] == "H":
+                    capture(hand2_army, hand2_prisoners, hand1_army, hand1_prisoners)
+                    self.game_txt += "Player 2 wins this hand!\n"
+                    self.game_txt += display_results(self)
 
             # It's a tie
-            elif hand1_army[0] == hand2_army[0]:
-                # Put army into prisoners for hand 1
-                hand1_prisoners.extend(hand1_army[0])
-                hand1_army.remove(hand1_army[0])
-                # Check for army size
-                if hand1_army != []:
-                    hand1_prisoners.extend(hand1_army[0])
-                    hand1_army.remove(hand1_army[0])
-                # Put army into prisoners for hand 2
-                hand2_prisoners.extend(hand2_army[0])
-                hand2_army.remove(hand2_army[0])
-                # Check for army size
-                if hand2_army != []:
-                    hand2_prisoners.extend(hand2_army[0])
-                    hand2_army.remove(hand2_army[0])
+            elif self.hand1_army[0] == self.hand2_army[0]:
+                    # Put army into prisoners for hand 1
+                    self.hand1_prisoners.extend(self.hand1_army[0])
+                    self.hand1_army.remove(self.hand1_army[0])
+                    # Check for army size
+                    if self.hand1_army != []:
+                        self.hand1_prisoners.extend(self.hand1_army[0])
+                        self.hand1_army.remove(self.hand1_army[0])
+                    # Put army into prisoners for hand 2
+                    self.hand2_prisoners.extend(self.hand2_army[0])
+                    self.hand2_army.remove(self.hand2_army[0])
+                    # Check for army size
+                    if self.hand2_army != []:
+                        self.hand2_prisoners.extend(self.hand2_army[0])
+                        self.hand2_army.remove(hand2_army[0])
 
-                text += "It's a tie!\n"
-                text += display_results()
+                    self.game_txt += "It's a tie!\n"
+                    self.game_txt += display_results()
 
         # Find's winner if one player has no army.
         # If neither player has an army, finds who has most amount of heads
-        if  hand1_army == [] and hand2_army == []:
-            hand1_heads = hand1_prisoners.count("H")
-            hand2_heads = hand2_prisoners.count("H")
-            if hand1_heads > hand2_heads:
-                text += "And the winner is... Player 1!"
-            elif hand2_heads > hand1_heads:
-                text += "And the winner is... Player 2!"
-            elif hand2_heads == hand1_heads:
-                text += "It's a tie! What are the chances of that?"
+        if  self.hand1_army == [] and self.hand2_army == []:
+            self.hand1_heads = self.hand1_prisoners.count("H")
+            self.hand2_heads = self.hand2_prisoners.count("H")
+            if self.hand1_heads > self.hand2_heads:
+                self.game_txt += "And the winner is... Player 1!"
+            elif self.hand2_heads > self.hand1_heads:
+                self.game_txt += "And the winner is... Player 2!"
+            elif self.hand2_heads == self.hand1_heads:
+                self.game_txt += "It's a tie! What are the chances of that?"
 
-        elif hand1_army == []:
-            text += "And the winner is... Player 2!"
+        elif self.hand1_army == []:
+            self.game_txt += "And the winner is... Player 2!"
 
-        elif hand2_army == []:
-            text += "And the winner is... Player 1!"
+        elif self.hand2_army == []:
+            self.game_txt += "And the winner is... Player 1!"
 
         # display the game
         self.game_txt.delete(0.0, END)
@@ -194,7 +196,7 @@ root.geometry("500x800")
 app = Frame(root).grid()
 
 # Instantiate object with the root window as its master.
-app = Application(root)
+app = CoinWar(root)
 
 # Run window's even loop.
 root.mainloop()
